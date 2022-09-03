ecd00e72685b615a76f010692029525173116bfd
OHDERIN <76110829+OHDERIN@users.noreply.github.com>
2022-09-03 07:11:58 -0500

    site files

---
+++ src/index.js
@@ -0,0 +1,16 @@
+var rickrolled = document.getElementById("rickroll");
+var coconut = document.getElementById("cocomalled")
+var grubhubvid = document.getElementById("grubhubvid")
+var fornitevi = document.getElementById("fortnite19")
+
+function playVid() {
+    rickrolled.play();
+    coconut.play();
+    grubhubvid.play();
+    fornitevi.play();
+}
+function showDiv() {
+    rickrolled.style.visibility = "visible";
+    coconut.style.visibility = "visible";
+    document.getElementById('gifs').style.visibility = "visible";
+}
